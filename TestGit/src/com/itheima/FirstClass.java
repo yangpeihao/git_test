c9697ea8eee928e3f5248e670dd3c555591cae5a
Wayne <wayne@itcast.com>
2019-02-17 21:43:29 +0800

    第一次提交

---
+++ TestGit/src/com/itheima/FirstClass.java
@@ -0,0 +1,10 @@
+package com.itheima;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("11111111111");
+    }
+    public void test(){
+        int i = 0;
+    }
+}
