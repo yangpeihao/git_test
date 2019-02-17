1b06acaa6114b8df28c217d8b28f136490bd4727
Wayne <wayne@itcast.com>
2019-02-17 21:54:49 +0800

    克隆

--- TestGit/src/com/itheima/FirstClass.java
+++ TestGit/src/com/itheima/FirstClass.java
@@ -3,8 +3,15 @@ package com.itheima;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("11111111111");
+        System.out.println("11111111111");
+        System.out.println("11111111111");
+        System.out.println("11111111111");
+        System.out.println("11111111111");
+        System.out.println("11111111111");
+        System.out.println("11111111111");
     }
     public void test(){
         int i = 0;
+        System.out.println(i);
     }
 }
